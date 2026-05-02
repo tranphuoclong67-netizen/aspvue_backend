3a1043174b518704e9f822a730b1464b5e0e87e2
tranphuoclong67-netizen <tranphuoclong67@gmail.com>
2026-05-02 20:50:16 +0700

    update CD-CI

--- obj/Debug/net8.0/ServerApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ServerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4352376a01a393032d755e75502163b94af32326")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f2e97d336d5d319e777ba402a33b7a0b880842d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
